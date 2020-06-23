c412c7c1e31d24378dd0f966642a9b1d61281e44
wding96 <57302294+wding96@users.noreply.github.com>
2020-06-22 22:15:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const fs = require("fs");
 const app = express();
 const port = 3001;
-const fileName = 'index_data.json';
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,26 +9,26 @@ app.listen(port, function(){
     console.log(`IncrementFunction listening on port ${port}`)
 });
 
+let history = {
+    1: {
+        "current_value" : 1,
+        "increment_value": 2
+    },
+}
 // Read
 app.get('/', function(req, res) {
-    let data = fs.readFileSync("index_data.json");
-    data = data.toString();
-    return res.send(Object.values(data));
+    return res.send(Object.values(history));
 });
 
 // Create 
 app.post('/', (req, res) => {
+    const id = req.body.value;
     const n = { 
         current_value : req.body.value,
         increment_value: req.body.value + 1
     };
-    let data = fs.readFileSync(fileName);
-    data = data.toString();
-    let Data = JSON.parse(data)
-    Data.push(n)
-    // fs.writeFileSync(__dirname + "/index_data.json", JSON.stringify(Data, null,2));
+    history[id] = n;
     return res.json(n);
-    // res.send(n);
 });
 // curl code to run: 
 // curl -X POST -H "Content-Type:application/json" http://localhost:3001/ -d '{"value":4}'
