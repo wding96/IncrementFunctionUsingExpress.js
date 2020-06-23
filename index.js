f63365ab97480617e3c96ccecd1ec25961813e84
wding96 <57302294+wding96@users.noreply.github.com>
2020-06-22 22:06:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3001;
+const fileName = 'index_data.json';
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get('/', function(req, res) {
-//     res.json(function(n){
-//         return n+1;
-//     })
-// });
-
 app.listen(port, function(){
-    console.log(`incrementFunction app listening on port ${port}`)
+    console.log(`IncrementFunction listening on port ${port}`)
 });
 
-// Create a message 
+// Read
+app.get('/', function(req, res) {
+    let data = fs.readFileSync("index_data.json");
+    data = data.toString();
+    return res.send(Object.values(data));
+});
+
+// Create 
 app.post('/', (req, res) => {
-    const n = { value : parseInt(req.body.value) + 1};
-    return res.send(n);
-    res.json(function(n){
-                return n+1;
-            })
+    const n = { 
+        current_value : req.body.value,
+        increment_value: req.body.value + 1
+    };
+    let data = fs.readFileSync(fileName);
+    data = data.toString();
+    let Data = JSON.parse(data)
+    Data.push(n)
+    // fs.writeFileSync(__dirname + "/index_data.json", JSON.stringify(Data, null,2));
+    return res.json(n);
+    // res.send(n);
 });
 // curl code to run: 
-// curl -X POST -H "Content-Type:application/json" http://localhost:3001/ -d '{"value":"4"}'
+// curl -X POST -H "Content-Type:application/json" http://localhost:3001/ -d '{"value":4}'
+
